7ed302cd3e94300b4aba88616039d30657f0a1ef
akila <akilandeswari.dhandapani@wipro.com>
2018-09-12 02:20:19 +0530

    added two test cases

--- selenium.js
+++ selenium.js
@@ -17,3 +17,62 @@ driver.get('http://localhost:3000/#/home').
 				return driver.quit();
 
 			});
+
+
+
+		driver.get('http://localhost:3000/#/home').
+	then(function(){
+			return driver.findElement( {xpath : '/html/body/div/div/div/header/div/a/button'}).click();
+		}).
+		then(function(){
+			return driver.getCurrentUrl();
+		}).
+		then( function(url){
+			assert.equal("http://localhost:3000/#/loadconf", url);
+		}).
+		then(function(){
+
+				console.log('successfully clicking on log button');
+				return driver.sleep(5000);
+
+			}).
+		then(function(){
+			return driver.quit();
+		});
+
+driver.get('http://localhost:3000/#/home').then(function(){
+			return driver.findElement( {xpath : '/html/body/div/div/div/header/div/a/button'}).click();
+}).then(function(){
+
+			return driver.findElement( {xpath : '/html/body/div/div/div/center/div/div/div/div'}).click();
+				}).then (function(){
+					return driver.findElement({xpath : '/html/body/div[2]/div[2]/ul/li[3]' }).click();
+				}).then(function(){
+					return driver.getCurrentUrl();
+				}).then( function(url){
+					assert.equal("http://localhost:3000/#/loadconf", url);
+				}).then( function(){
+					console.log('successfully clicking dropdown option');
+					return driver.sleep(5000);
+			}).then(function(){
+					return driver.quit();
+				});
+
+
+driver.get('http://localhost:3000/#/home').then(function(){
+			return driver.findElement( {xpath : '/html/body/div/div/div/header/div/a/button'}).click();
+}).then(function(){
+
+			return driver.findElement( {xpath : '/html/body/div/div/div/center/div/div/div/div'}).click();
+				}).then(function(){
+					return driver.getCurrentUrl();
+				}).then( function(url){
+					assert.equal("http://localhost:3000/#/loadconf", url);
+				}).then( function(){
+					console.log('successfully view dropdown option');
+					return driver.sleep(5000);
+			}).then(function(){
+					return driver.quit();
+				});
+
+
